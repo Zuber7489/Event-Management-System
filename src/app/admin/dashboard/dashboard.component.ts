29ef0f6d47b770fbf51117c18d946c0427e87695
MOHAMMAD ZUBER <zs808081@gmail.com>
2023-03-09 03:21:28 -0800

    'newcode'

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -11,19 +11,9 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  public data: Object[] = [{ x: 'WW', y: 38.3, text: 'World Wide' },
-  { x: 'EU', y: 45.2, text: 'Europe' },
-  { x: 'APAC', y: 18.2, text: 'Asia Pacific' },
-  { x: 'LATAM', y: 46.7, text: 'Latin America' },
-  { x: 'MEA', y: 61.5, text: 'Middle East Africa' },
-  { x: 'NA', y: 64, text: 'North America' }];
-
-  public primaryXAxis: Object = {
-    valueType: 'Category',
-     
-};
-
-  allbooking:any;
+ 
+count:number=1;
+  
 userData:any;
 token = sessionStorage.getItem('adminDetail')
   constructor(private router: Router, private toastr: ToastrService, 
@@ -32,33 +22,27 @@ token = sessionStorage.getItem('adminDetail')
      }
 
   ngOnInit(): void {
-    this.dashboardCount();
-    this.dashboardcounter();
-   
+    this.count++
+    this.http.get('https://event-r2eh.onrender.com/employee/signup/alldata', {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': `Bearer ` + this.token
+      })
+    }).subscribe(res=>{
+this.userData = res
+console.log(this.userData);
+
+    })
   }
  
-  dashboardCount() {
-    this.http.get('https://event-r2eh.onrender.com/employee/eventlist/data').subscribe(res=>{
-
-console.log(res)
-this.allbooking = res;
-   })
+  
 
    
   }
 
-dashboardcounter(){
-  this.http.get('https://event-r2eh.onrender.com/employee/signup/alldata', {
-    headers: new HttpHeaders({
-      'Accept': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Authorization': `Bearer ` + this.token
-    })
-  }).subscribe(res=>{
-this.userData = res
-  })
-}
 
 
 
-}
+
+
