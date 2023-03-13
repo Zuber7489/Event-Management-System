38a24cd3991f3da6e33c9cc7e4cc8414bc16795f
MOHAMMAD ZUBER <zs808081@gmail.com>
2023-03-12 23:51:13 -0700

    'new'

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -8,41 +8,63 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
- 
-count:number=1;
-  
-userData:any;
-token = sessionStorage.getItem('adminDetail')
-  constructor(private router: Router, private toastr: ToastrService, 
-    private route: ActivatedRoute, private ThreeDService: ThreeDServiceService,public http:HttpClient) {
-      
-     }
+  c: number = 0;
+  Pro:number=0;
+  Premium:number=0;
+  allData: any;
+  userData: any;
+  token = sessionStorage.getItem('adminDetail');
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private route: ActivatedRoute,
+    private ThreeDService: ThreeDServiceService,
+    public http: HttpClient
+  ) {}
 
+  userCount:number=0;
+  bookingCount:number=0;
   ngOnInit(): void {
-    this.count++
-    this.http.get('https://event-r2eh.onrender.com/employee/signup/alldata', {
-      headers: new HttpHeaders({
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Authorization': `Bearer ` + this.token
+    this.http
+      .get('https://event-r2eh.onrender.com/employee/signup/alldata', {
+        headers: new HttpHeaders({
+          Accept: 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          Authorization: `Bearer ` + this.token,
+        }),
       })
-    }).subscribe(res=>{
-this.userData = res
-console.log(this.userData);
+      .subscribe((res) => {
+        this.userData = res;
+       this.userCount =this.userData.length;
+      });
 
-    })
+    this.http
+      .get('https://event-r2eh.onrender.com/employee/eventlist/data')
+      .subscribe((res) => {
+        console.log(res);
+        this.allData = res;
+       
+            
+          this.allData.forEach((element: { plan: any; }) => {
+            if (element.plan  == "Basic") {
+              this.c = this.c + 1;
+            }else if(element.plan == 'Pro'){
+              this.Pro = this.Pro + 1;
+            }else if(element.plan=='Premium'){
+              this.Premium= this.Premium + 1;
+            }
+            
+            
+          });
+        
+        this.bookingCount =this.allData.length
+      });
   }
- 
-  
-
-   
-  }
-
-
-
-
-
+}
+function elseIf(arg0: boolean) {
+  throw new Error('Function not implemented.');
+}
 
